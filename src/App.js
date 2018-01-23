232d51d5fd4305123708d9eecf1347a110f5b60f
Guilherme Tassinari <guitassinari@gmail.com>
2018-01-23 18:11:22 -0200

    sadds spinner

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import Button from './components/RaisedButton'
 import OpportunityCard from './components/OpportunityCard'
 import Header from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
 import Typography from 'material-ui/Typography'
 import Grid from 'material-ui/Grid'
+import Spinner from 'react-spinkit'
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      opportunities: []
+      opportunities: [],
+      loading: false,
     }
   }
 
   componentWillMount(){
+    this.setState({ loading: true })
     fetch('https://we-are-ubuntu.firebaseio.com/opportunities.json')
       .then(response => response.json())
-      .then(opportunities => this.setState({ opportunities }))
+      .then(opportunities => this.setState({ opportunities, loading: false }))
   }
 
   render() {
     return (
       <div>
-        <Header style={{backgroundColor: 'white'}} position="sticky">
+        <Header style={{backgroundColor: 'white'}} position="static">
           <Toolbar>
             <Typography type="title" >
               We Are Ubuntu
@@ -33,8 +35,9 @@ class App extends Component {
         </Header>
         <div className="BaseContainer">
           <Grid container >
-            {this.state.opportunities.length === 0 ? <h1>Nenhuma oportunidade no momento</h1> : null}
 
+            {this.state.loading ? <Grid item xs={12} style={{display: 'flex', justifyContent: 'center'}}><Spinner style={{height: 62, width: 62}} name="circle" color="green" /></Grid> : null}
+            {this.state.opportunities.length === 0 && !this.state.loading ? <h1>Nenhuma oportunidade no momento</h1> : null}
             {this.state.opportunities.map((opp, index) => (
               <Grid key={index} item md={6} sm={6} xs={12}>
                 <OpportunityCard {...opp}/>
--- src/components/OpportunityCard.js
+++ src/components/OpportunityCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Card, { CardContent, CardHeader } from 'material-ui/Card'
-import Grid from 'material-ui/Grid'
 import Typography from 'material-ui/Typography'
 
 export default function({title, description}){
