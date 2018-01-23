50011def92f3f2e7d073232dd63666eccdb86def
Guilherme Tassinari <guitassinari@gmail.com>
2018-01-23 18:12:23 -0200

    Opportunities list (#5)
    * integrates with firebase
    
    * sadds spinner

--- src/components/OpportunityCard.js
+++ src/components/OpportunityCard.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Card, { CardContent, CardHeader } from 'material-ui/Card'
-import Grid from 'material-ui/Grid'
 import Typography from 'material-ui/Typography'
 
-export default function(){
+export default function({title, description}){
   return (
     <Card>
-      <CardHeader style={{backgroundColor: "#61BF0F"}} title="Titulo da vaga" />
+      <CardHeader style={{backgroundColor: "#61BF0F"}} title={title} />
       <CardContent>
         <Typography type="body2">
-          Descricao da vaga
+          {description}
         </Typography>
       </CardContent>
     </Card>
--- src/components/RaisedButton.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import Button from 'material-ui/Button'
-
-export default props => <Button raised {...props} />
