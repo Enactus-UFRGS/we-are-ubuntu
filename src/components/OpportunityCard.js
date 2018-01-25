20b610c07a4e2637851db3d01484714cec8b5667
Guilherme Tassinari <guitassinari@gmail.com>
2018-01-24 22:09:52 -0200

    op card modal

--- src/components/OpportunityCard.js
+++ src/components/OpportunityCard.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react'
 import Card, { CardContent, CardHeader } from 'material-ui/Card'
 import Typography from 'material-ui/Typography'
+import Modal from 'material-ui/Modal'
 
-export default function({title, description}){
-  return (
-    <Card>
-      <CardHeader style={{backgroundColor: "#61BF0F"}} title={title} />
-      <CardContent>
-        <Typography type="body2">
-          {description}
-        </Typography>
-      </CardContent>
-    </Card>
-  )
+const OpCard = ({title, description}) => (
+  <Card>
+    <CardHeader style={{backgroundColor: "#61BF0F"}} title={title} />
+    <CardContent>
+      <Typography type="body2" style={{maxHeight: 300, overflow: 'scroll'}}>
+        {description}
+      </Typography>
+    </CardContent>
+  </Card>
+)
+
+export default class OpportunityCard extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      showModal: false
+    }
+  }
+
+  closeModal(){
+    this.setState({showModal: false})
+  }
+
+  openModal(){
+    this.setState({showModal: true})
+  }
+
+  render(){
+    return (
+      <div>
+        <Modal open={this.state.showModal} onClose={() => this.closeModal()} className="ModalBase" >
+          <OpCard {...this.props}/>
+        </Modal>
+        <div onClick={() => this.openModal()}>
+          <OpCard {...this.props}  />
+        </div>
+      </div>
+    )
+  }
 }
