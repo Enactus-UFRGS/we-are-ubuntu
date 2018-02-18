f8b3699ddb0312afea4e51c3ba28f0bc4a7f9038
Guilherme Tassinari <guitassinari@gmail.com>
2018-02-18 13:22:47 -0300

    adds routes

--- src/App.js
+++ src/App.js
@@ -1,90 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import OpportunityCard from './components/OpportunityCard/OpportunityCard'
-import Grid from 'material-ui/Grid'
-import Spinner from 'react-spinkit'
-import Header from './components/Header/Header'
-import Button from 'material-ui/Button'
-import AddIcon from 'material-ui-icons/Add'
-import firebase from 'firebase'
-import { onValueChange } from './model/opportunity'
-import OpportunityForm from './components/OpportunityForm'
-import { OPPORTUNITY_TYPES_COLORS, OPPORTUNITY_TYPES } from "./model/opportunity";
+import { Provider } from 'react-redux'
+import { ConnectedRouter, push } from 'react-router-redux'
+import { Route } from 'react-router'
+import { store, history } from './store'
+import Home from './routes/Home'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      opportunities: [],
-      loading: true,
-      openLoginModal: false,
-      openDialog: false,
-      logged: false
-    }
-    onValueChange(opportunities => {
-      this.setState({
-        opportunities,
-        loading: false
-      })
-    })
-    setTimeout(() => {
-      firebase.auth().onAuthStateChanged(user => {
-        this.setState({ logged: !!user })
-      });
-    })
-  }
-
-  openFormDialog() {
-    this.setState({ openDialog: true })
-  }
-
-  closeFormDialog() {
-    this.setState({ openDialog: false })
-  }
 
   render() {
-    return (<div>
-      <Header/>
-      <div className="BaseContainer">
-        <ul className="OpportunitiesLegend">
-          <li>
-            <div style={{backgroundColor: OPPORTUNITY_TYPES_COLORS[OPPORTUNITY_TYPES.JOB]}} className="LegendCircle"></div>
-            Vaga de emprego
-          </li>
-          <li>
-            <div style={{backgroundColor: OPPORTUNITY_TYPES_COLORS[OPPORTUNITY_TYPES.HOME]}} className="LegendCircle"></div>
-            Oferta de moradia
-          </li>
-        </ul>
-        <Grid container>
-
-          { this.state.loading ? <Grid item xs={ 12 } style={ {
-            display: 'flex',
-            justifyContent: 'center'
-          } }><Spinner
-            style={ {
-              height: 62,
-              width: 62
-            } } name="circle" color="green"/></Grid> : null }
-          { this.state.opportunities.length === 0 && !this.state.loading ?
-            <h1>Nenhuma oportunidade no momento</h1> : null }
-          { this.state.opportunities.map((opp, index) => (<Grid key={ index } item md={ 3 } sm={ 6 } xs={ 12 }>
-            <OpportunityCard { ...opp }/>
-          </Grid>)) }
-
-        </Grid>
-        { this.state.logged ? (<Button onClick={ () => this.openFormDialog() } fab style={ {
-          position: 'fixed',
-          right: 32,
-          bottom: 32
-        } }
-                                       color="primary">
-          <AddIcon/>
-        </Button>) : null }
-
-        <OpportunityForm open={ this.state.openDialog } onClose={ () => this.closeFormDialog() }/>
-      </div>
-    </div>);
+    return (
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <div>
+            <Route exact path="/" component={Home}/>
+          </div>
+        </ConnectedRouter>
+      </Provider>
+    );
   }
 }
 
---
+++ src/store.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+
+import createHistory from 'history/createBrowserHistory'
+import { Route } from 'react-router'
+
+import { routerReducer, routerMiddleware, push } from 'react-router-redux'
+
+
+// Create a history of your choosing (we're using a browser history in this case)
+export const history = createHistory()
+
+// Build the middleware for intercepting and dispatching navigation actions
+const middleware = routerMiddleware(history)
+
+// Add the reducer to your store on the `router` key
+// Also apply our middleware for navigating
+
+const reducers = {}
+
+export const store = createStore(
+  combineReducers({
+    ...reducers,
+    router: routerReducer
+  }),
+  applyMiddleware(middleware)
+)
